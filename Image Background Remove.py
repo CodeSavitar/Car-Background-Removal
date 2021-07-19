fe4ae73af1f2ed7cec5b2ba8374119d1aa3eb51e
CodeSavitar <adhithsagit0510@gmail.com>
2021-07-19 20:47:42 +0530

    images added

--- Image Background Remove.py
+++ Image Background Remove.py
@@ -16,7 +16,7 @@ mask = cv2.inRange(hsv, (0, 0, 200), (179, 100, 255))
 
 white = cv2.bitwise_and(resized,resized, mask= mask)
 
-cv2.imshow('Image', mask)
+cv2.imshow('Image', white)
 cv2.waitKey(0)
 
 #(0, 0, 200), (179, 100, 255)
--- Image Background Replace.py
+++ Image Background Replace.py
@@ -9,7 +9,7 @@ width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-print('Resized Dimensions : ',resized.shape)
+# print('Resized Dimensions : ',resized.shape)
 
 white_img, masked_img = get_masked_image(resized)
 
