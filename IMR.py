e5488ca8625cddf411e2b4df60dc33a5e0c48227
Adhith Sankar <52819770+CodeSavitar@users.noreply.github.com>
2021-07-24 00:03:49 +0530

    Update IMR.py

--- IMR.py
+++ IMR.py
@@ -5,5 +5,5 @@ def get_masked_image(resized):
 
     hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, (0, 0, 200), (179, 100, 255))
-    white = cv2.bitwise_and(resized,resized, mask= mask)
+    white = cv2.bitwise_and(resized, resized, mask = mask)
     return white, mask
